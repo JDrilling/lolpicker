45d000d24747fa8d6225aec58545eab4a385c8fe
Jacob Drilling <jcdrilling@gmail.com>
2017-02-17 00:01:36 -0800

    Add basic login form

--- picker/urls.py
+++ picker/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from django.contrib.auth import views as authViews
 from picker.views import IndexView, GameView, NewGameView
 
 urlpatterns = [
     url(r'^$', IndexView.as_view()),
     url(r'^game/(?P<pk>[0-9]+)/$', GameView.as_view(), name="game-view"),
     url(r'^new/game/$', NewGameView.as_view(), name="new-game-view"),
+    url(r'^login/$', authViews.login, name="login"),
+    url(r'^logout/$', authViews.logout, {'next_page': '/'}, name="logout"),
 ]
